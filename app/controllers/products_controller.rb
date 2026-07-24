3d65aa87147e9aa0bfc40883c027c300c4c6acab
fumnanya12 <[fumnanya.obi@gmail.com](mailto:fumnanya.obi@gmail.com)>
2026-07-24 02:36:36 -0500

    Implementation of the product filter and product view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,68 @@
 class ProductsController < ApplicationController
   def index
-    @products=Product.all
+    @categories = Category.order(:name)
+    @products = Product.includes(:category)
+
+    if params[:search].present?
+      keyword = "%#{ActiveRecord::Base.sanitize_sql_like(params[:search])}%"
+
+      @products = @products.where(
+        "products.name LIKE :keyword OR products.description LIKE :keyword",
+        keyword: keyword
+      )
+      @products_search=@products.where(
+        "products.name LIKE :keyword OR products.description LIKE :keyword",
+        keyword: keyword
+      ).page(params[:page]).per(20)
+    end
+
+    if params[:category_id].present?
+      @selected_category = Category.find(params[:category_id])
+      @products = @products.where(category_id: params[:category_id])
+      @products_search= @products.where(category_id: params[:category_id]).page(params[:page]).per(20)
+    end
+  case params[:filter]
+  when "available"
+    @products = @products.where(avaliable: true)
+    @products_search=@products
+  when "new"
+    @products = @products.where(created_at: 3.days.ago..Time.current)
+    @products_search=@products
+  when "recently_updated"
+    @products = @products
+      .where(updated_at: 3.days.ago..Time.current)
+      .where("created_at < ?", 3.days.ago)
+    @products_search=@products
+  end
+
+
+    @products = @products.page(params[:page]).per(9)
+    @products_search=@products
   end
 
   def show
-    @products=Product.find(params[:id])
+    @categories = Category.order(:name)
+
+    @product = Product.find(params[:id])
   end
 end
+#   def index
+#     @products_search=[]
+#     @products=[]
+#     if params[:query].present?
+#       search = "%#{params[:query]}%"
+#       @products_search=Product.where(
+#         "name LIKE :search",
+#         search: search
+#       ).page(params[:page])
+#       .per(30)
+#     else
+#       @products=Product.all
+#      .page(params[:page])
+#       .per(30)
+#     end
+#   end
+
+#   def show
+#     @products=Product.find(params[:id])
+#   end
